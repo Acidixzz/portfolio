9507ee3fc689f036f58f6d97cc21385fb60da61f
Owen W <chasemccainn@aol.com>
2025-01-25 01:40:28 -0800

    added dark mode toggle kind of janky and might remove if i come up with a theme

--- my-portfolio/src/components/Nav.tsx
+++ my-portfolio/src/components/Nav.tsx
@@ -1,9 +1,12 @@
 import { useLocation } from "@solidjs/router";
+import DarkModeToggle from "./DarkModeToggle";
 
 export default function Nav() {
   const location = useLocation();
+
   const active = (path: string) =>
     path == location.pathname ? "border-sky-600" : "border-transparent hover:border-sky-600";
+
   return (
     <nav class="bg-sky-800">
       <ul class="container flex items-center p-3 text-gray-200">
@@ -12,6 +15,9 @@ export default function Nav() {
         </li>
         <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
           <a href="/about">About</a>
+        </li>
+        <li>
+          <DarkModeToggle />
         </li>
       </ul>
     </nav>
--- my-portfolio/src/entry-server.tsx
+++ my-portfolio/src/entry-server.tsx
@@ -6,6 +6,16 @@ export default createHandler(() => (
     document={({ assets, children, scripts }) => (
       <html lang="en">
         <head>
+          <script>
+            {`(function() {
+        const theme = localStorage.getItem('theme'); 
+        if (theme === 'dark') {
+          document.documentElement.classList.add('dark');
+        } else {
+          document.documentElement.classList.remove('dark');
+        }
+      })();`}
+          </script>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />

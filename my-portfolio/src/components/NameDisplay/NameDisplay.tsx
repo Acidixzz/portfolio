883329b5297a63ed96354499147d4aff60569102
Acidixzz <chasemccainn@aol.com>
2025-01-27 09:13:12 -0800

    turn off the cursor follow for mobile devices

--- my-portfolio/src/components/NameDisplay/NameDisplay.tsx
+++ my-portfolio/src/components/NameDisplay/NameDisplay.tsx
@@ -4,35 +4,41 @@ import "./NameDisplay.css";
 
 export default function NameDisplay() {
 
-    // const [isMobile, setIsMobile] = createSignal();
+    const [isMobile, setIsMobile] = createSignal();
 
-    // onMount(() => {
-    //     setIsMobile(window.innerWidth <= 750);
+    onMount(() => {
+        setIsMobile(window.innerWidth <= 750);
 
-    //     window.addEventListener("resize", () => {
-    //         setIsMobile(window.innerWidth <= 750);
-    //     });
-    // });
+        window.addEventListener("resize", () => {
+            setIsMobile(window.innerWidth <= 750);
+        });
+    });
 
     return (
         <div>
-            <Tilt
-                options={{
-                    max: 30,
-                    speed: 1000,
-                    reverse: true,
-                    transition: true,
-                    gyroscope: true,
-                    gyroscopeMinAngleX: -30,
-                    gyroscopeMaxAngleX: 30,
-                    gyroscopeMinAngleY: -30,
-                    gyroscopeMaxAngleY: 30,
-                    "full-page-listening": true
-                }}
-                class="mx-auto tilt-size flex items-center justify-center text-center"
-            >
-                <h1 class="uppercase name-text">Owen Wilson</h1>
-            </Tilt>
+            {isMobile() ?
+                (
+                    <h1 class="uppercase name-text">Owen Wilson</h1>
+                )
+                :
+                (<Tilt
+                    options={{
+                        max: 30,
+                        speed: 1000,
+                        reverse: true,
+                        transition: true,
+                        gyroscope: true,
+                        gyroscopeMinAngleX: -30,
+                        gyroscopeMaxAngleX: 30,
+                        gyroscopeMinAngleY: -30,
+                        gyroscopeMaxAngleY: 30,
+                        "full-page-listening": true
+                    }}
+                    class="mx-auto tilt-size flex items-center justify-center text-center"
+                >
+                    <h1 class="uppercase name-text">Owen Wilson</h1>
+                </Tilt>)
+            }
             <div class="graph-container">
                 <div class="blur-line"></div>
                 <div class="graph"></div>

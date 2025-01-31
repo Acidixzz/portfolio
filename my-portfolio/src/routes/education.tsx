194c90d86284af34df652fabd45218aaa324d760
Owen W <chasemccainn@aol.com>
2025-01-31 01:00:15 -0800

    added uofo text

--- my-portfolio/src/routes/education.tsx
+++ my-portfolio/src/routes/education.tsx
@@ -1,7 +1,9 @@
-
+import "./education.css";
 
 export default function Education() {
     return (
-        <div></div>
+        <div class="justify-center items-center" style={"height: 100vh; margin-top: 10vh"}>
+            <p class="university-text">University of Oregon</p>
+        </div>
     )
 }

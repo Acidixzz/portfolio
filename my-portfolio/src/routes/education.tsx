a8aa0f9799e37c694e388b7c7e88c34eec6eb733
Owen W <chasemccainn@aol.com>
2025-01-31 15:21:18 -0800

    fixed some issues with the education page and the navigation bar

--- my-portfolio/src/routes/education.tsx
+++ my-portfolio/src/routes/education.tsx
@@ -3,8 +3,8 @@ import "./education.css";
 
 export default function Education() {
     return (
-        <main class="text-center mx-auto main-size">
-            <div class="mx-auto flex flex-col items-center text-center tile-container">
+        <main class="text-center mx-auto">
+            <div class="mx-auto flex flex-col items-center text-center education-main">
                 <p class="university-text" style={"margin-bottom: 10vh;"} data-text="University of Oregon">University of Oregon</p>
                 <UOTextBox>
                 </UOTextBox>

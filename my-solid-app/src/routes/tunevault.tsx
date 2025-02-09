cdb697dff7a560db8020e0cc8cc0230b89ad9781
Owen W <chasemccainn@aol.com>
2025-02-09 05:22:59 -0800

    clean upp the tunevault words a little bit

--- my-solid-app/src/routes/tunevault.tsx
+++ my-solid-app/src/routes/tunevault.tsx
@@ -1,6 +1,7 @@
 import PhoneVideoMockup from "../components/PhoneVideoMockup/PhoneVideoMockup";
 import TextBox from "../components/TextBox/TextBox";
 import UOTextBox from "../components/UOTextBox/UOTextBox";
+import "./tunevault.css";
 
 
 export default function TuneVault() {
@@ -16,21 +17,29 @@ export default function TuneVault() {
                 <div style={"border-width: 0.5px; margin: 10px; border-color: var(--skyblue); margin-left: 30%; margin-right: 30%; box-shadow: 0 0 10px var(--skyblue-transparent);"} />
                 <div class="image-uo" style={"--bg-image: url('/projects/tune-mock.png'); transform: scale(1)"} />
                 <p style={"padding: 30px; color: white;"}>
-                    Tune Vault Is an MP3 player mobile app with a modern feel on top of a friendly user interface.
-                    Just tap on the plus icon in the songs tab to import MP3 files directly from your device!
-                </p>
-                <p style={"padding: 30px; color: white;"}>
-                    After songs have been imported, you have the freedom to edit that song to your heart's content!
-                    In the edit song screen, you can chenge the Name, Artist, Picture, and even the background color of the song.
+                    Tune Vault is a modern MP3 player app designed with a sleek look and an intuitive user interface.
+                    Importing your favorite MP3 files is effortless—just tap the plus
+                    icon in the Songs tab to add music directly from your device! Once
+                    imported, you have complete control over customization:
                 </p>
 
-                <PhoneVideoMockup path="../../public/projects/color.mp4" />
+                <div class="flex">
+                    <PhoneVideoMockup path="../../public/projects/color.mp4" />
+                    <ul class="text-left spaced-list" style={"color: white; width: 100%; margin-top: 20px"}>
+                        <li>● Rename the song</li>
+                        <li>● Update the artist</li>
+                        <li>● Change the cover image</li>
+                        <li>● Personalize the background color</li>
+                    </ul>
+                </div>
 
                 <p style={"padding: 30px; color: white;"}>
-                    My goal with Tune Vault is to create an easy to use and personalizable experience for everyone. I have always wanted the ability
-                    to edit song covers and have each playlist represent who I am with more than just the song itself.
-                    Certain songs make me feel a certain way and I have a perfect image to faithfully represent that. With Tune Vault, you
-                    can make each playlist or individual song even more personal than other MP3 player apps out there.
+                    I designed Tune Vault to provide a seamless and deeply personal music experience.
+                    I've always wanted a way to connect a picture with a song that holds special
+                    meaning—whether it's tied to a cherished memory or evokes a powerful emotion.
+                    With Tune Vault, you can do just that. By linking images to individual songs
+                    or playlists, you can create a listening experience that feels more personal
+                    and immersive than any other MP3 player app.
                 </p>
 
             </UOTextBox>

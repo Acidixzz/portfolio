2ca72a8403b7cadcaa63d2a783dadbb234f61eaf
Owen W <chasemccainn@aol.com>
2025-02-09 18:08:29 -0800

    cahnged the scroll button style, made the videos more performant by turning them into webm files

--- my-solid-app/src/components/PhoneVideoMockup/PhoneVideoMockup.tsx
+++ my-solid-app/src/components/PhoneVideoMockup/PhoneVideoMockup.tsx
@@ -1,12 +1,15 @@
+import { onMount } from "solid-js";
+
 interface MockupProps {
     path: string;
 }
 
 export default function PhoneVideoMockup(props: MockupProps) {
+
     return (
         <div class="image-uo" style={"--bg-image: url('/projects/phone.png'); transform: scale(1)"} >
-            <video autoplay muted loop playsinline class="bg-video">
-                <source src={props.path} type="video/mp4" />
+            <video autoplay muted loop playsinline preload="auto" class="bg-video">
+                <source src={props.path} type="video/webm" />
                 Your browser does not support HTML5 video.
             </video>
         </div>
--- my-solid-app/src/routes/tunevault.tsx
+++ my-solid-app/src/routes/tunevault.tsx
@@ -7,24 +7,24 @@ import "./tunevault.css";
 export default function TuneVault() {
 
     return (
-        <div class="mx-auto flex flex-col items-center text-center main-view">
+        <div class="mx-auto flex flex-col items-center text-left main-view">
 
 
             <UOTextBox forTuneVault>
-                <h1 class="header-text" style={"padding-bottom: 0; padding-top: 10px"}>
+                <h1 class="header-text text-center" style={"padding-bottom: 0; padding-top: 10px;"}>
                     Tune Vault
                 </h1>
                 <div style={"border-width: 0.5px; margin: 10px; border-color: var(--skyblue); margin-left: 30%; margin-right: 30%; box-shadow: 0 0 10px var(--skyblue-transparent);"} />
                 <div class="image-uo" style={"--bg-image: url('/projects/tune-mock.png'); transform: scale(1)"} />
-                <p style={"padding: 30px; color: white;"}>
+                <p style={"padding: 30px; color: white; font-weight: 200;"}>
                     Tune Vault is a modern MP3 player app designed with a sleek look and an intuitive user interface.
-                    Importing your favorite MP3 files is effortless—just tap the plus
+                    Importing your favorite MP3 files is effortless. Just tap the plus
                     icon in the Songs tab to add music directly from your device! Once
                     imported, you have complete control over customization:
                 </p>
 
                 <div class="flex">
-                    <PhoneVideoMockup path="../../public/projects/color.mp4" />
+                    <PhoneVideoMockup path="../../public/projects/color.webm" />
                     <ul class="text-left spaced-list" style={"color: white; width: 100%; margin-top: 20px"}>
                         <li>● Rename the song</li>
                         <li>● Update the artist</li>
@@ -33,14 +33,21 @@ export default function TuneVault() {
                     </ul>
                 </div>
 
-                <p style={"padding: 30px; color: white;"}>
-                    I designed Tune Vault to provide a seamless and deeply personal music experience.
-                    I've always wanted a way to connect a picture with a song that holds special
-                    meaning—whether it's tied to a cherished memory or evokes a powerful emotion.
-                    With Tune Vault, you can do just that. By linking images to individual songs
-                    or playlists, you can create a listening experience that feels more personal
-                    and immersive than any other MP3 player app.
+                <p style={"padding: 30px; color: white; font-weight: 200;"}>
+                    Unlike any other MP3 player app, Tune Vault introduces features that enhance both convenience and personalization.
+                    I've designed it so users can tailor their experience by showing or hiding specific controls on the footer—such as shuffle,
+                    previous, play, and next—giving them complete control over their interface. This level of customization ensures that the player
+                    adapts to the user's preferences, making the listening experience more intuitive and personal.
                 </p>
+
+                <PhoneVideoMockup path="../../public/projects/settings.webm" />
+
+                <p style={"padding: 30px; color: white; font-weight: 200;"}>
+                    I designed Tune Vault to offer a seamless and highly personal music experience.
+                    Frustrated with the limitations of other music player apps, I set out to create a
+                    user experience that aligns with what I and many others truly want.
+                </p>
+
 
             </UOTextBox>
         </div>
